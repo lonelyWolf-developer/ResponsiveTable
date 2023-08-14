60657452e555ca2ea2167a3f3696ce8cc1e4f5ee
Vlk samotář <spravce@vlksamotar.cz>
2023-08-14 18:54:22 +0200

    Creating a index.php and style.css files.

---
+++ index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Responzive table</title>
+</head>
+<body>
+    <div class="container">
+        <div class="tableBox">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Lorem, ipsum.</th>
+                        <th>Magnam, atque.</th>
+                        <th>Voluptates, animi.</th>
+                        <th>Eum, quasi?</th>
+                        <th>Porro, sint.</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>Lorem, ipsum dolor.</td>
+                        <td>Eos, repellendus eum?</td>
+                        <td>Hic, dicta tenetur?</td>
+                        <td>Repellendus, et iste.</td>
+                        <td>Mollitia, nobis molestias.</td>
+                    </tr>
+                    <tr>
+                        <td>Lorem, ipsum dolor.</td>
+                        <td>Error, molestiae quisquam!</td>
+                        <td>Nesciunt, obcaecati ducimus.</td>
+                        <td>Laborum, et ipsum.</td>
+                        <td>Obcaecati, quasi aperiam!</td>
+                    </tr>
+                    <tr>
+                        <td>Lorem, ipsum dolor.</td>
+                        <td>Hic, delectus esse.</td>
+                        <td>Ex, eos. Nostrum?</td>
+                        <td>Eum, pariatur temporibus?</td>
+                        <td>Fugit, ab mollitia.</td>
+                    </tr>
+                    <tr>
+                        <td>Lorem, ipsum dolor.</td>
+                        <td>Ad, repellat distinctio?</td>
+                        <td>Rerum, commodi quia?</td>
+                        <td>Quidem, id delectus!</td>
+                        <td>Numquam, enim tempore!</td>
+                    </tr>
+                    <tr>
+                        <td>Lorem, ipsum dolor.</td>
+                        <td>Maxime, repellat. Magni.</td>
+                        <td>Id, sunt temporibus.</td>
+                        <td>Sit, explicabo excepturi!</td>
+                        <td>Ut, aliquam quos?</td>
+                    </tr>
+                    <tr>
+                        <td>Lorem, ipsum dolor.</td>
+                        <td>Deserunt, aspernatur recusandae!</td>
+                        <td>Quidem, nihil porro!</td>
+                        <td>Ipsa, voluptate illo.</td>
+                        <td>Esse, quae aut!</td>
+                    </tr>
+                    <tr>
+                        <td>Lorem, ipsum dolor.</td>
+                        <td>Mollitia, et dolor.</td>
+                        <td>Maxime, consequuntur repellat.</td>
+                        <td>Quae, illo accusantium.</td>
+                        <td>Dolorem, voluptate tempore.</td>
+                    </tr>
+                    <tr>
+                        <td>Lorem, ipsum dolor.</td>
+                        <td>Repudiandae, nobis molestias.</td>
+                        <td>Tempora, perferendis consequatur?</td>
+                        <td>Ipsam, tenetur numquam!</td>
+                        <td>Aliquid, dolor quis?</td>
+                    </tr>
+                    <tr>
+                        <td>Lorem, ipsum dolor.</td>
+                        <td>Praesentium, accusantium quisquam?</td>
+                        <td>Vero, atque iusto?</td>
+                        <td>Nemo, ea ratione.</td>
+                        <td>Sequi, a possimus?</td>
+                    </tr>
+                    <tr>
+                        <td>Lorem, ipsum dolor.</td>
+                        <td>Id, harum veniam.</td>
+                        <td>Quod, accusantium nemo.</td>
+                        <td>Dolores, molestias assumenda!</td>
+                        <td>Ratione, corporis! Nostrum.</td>
+                    </tr>
+                    <tr>
+                        <td>Lorem, ipsum dolor.</td>
+                        <td>Excepturi, quibusdam iste.</td>
+                        <td>Quisquam, sapiente esse!</td>
+                        <td>Hic, harum nobis.</td>
+                        <td>Sed, mollitia beatae!</td>
+                    </tr>
+                    <tr>
+                        <td>Lorem, ipsum dolor.</td>
+                        <td>Maiores, sequi veniam.</td>
+                        <td>Et, temporibus dolores?</td>
+                        <td>Libero, itaque totam?</td>
+                        <td>Perferendis, error porro.</td>
+                    </tr>
+                    <tr>
+                        <td>Lorem, ipsum dolor.</td>
+                        <td>Aspernatur, quos animi!</td>
+                        <td>Ipsa, expedita quis.</td>
+                        <td>Dolorem, modi voluptatum?</td>
+                        <td>Aliquid, quasi illo.</td>
+                    </tr>
+                    <tr>
+                        <td>Lorem, ipsum dolor.</td>
+                        <td>Voluptas, quas cum.</td>
+                        <td>Libero, aspernatur sapiente?</td>
+                        <td>Commodi, sequi obcaecati.</td>
+                        <td>Fugiat, inventore pariatur.</td>
+                    </tr>
+                    <tr>
+                        <td>Lorem, ipsum dolor.</td>
+                        <td>Nemo, similique reprehenderit?</td>
+                        <td>Impedit, minima quo!</td>
+                        <td>Libero, reprehenderit velit!</td>
+                        <td>Temporibus, qui similique?</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</body>
+</html>
